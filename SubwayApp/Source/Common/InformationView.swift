d224a6626e3531fe12654c506b1bda8242443f3c
김영균 <kyoungyun98@naver.com>
2022-07-08 15:09:24 +0900

    feat: 지하철 정보 뷰 추가
    - 지하철의 목적지, 시간, 혼잡도 아이콘 있는 뷰

---
+++ SubwayApp/Source/Common/InformationView.swift
@@ -0,0 +1,55 @@
+//
+//  InformationView.swift
+//  SubwayApp
+//
+//  Created by 김영균 on 2022/07/07.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class InformationView: UIView {
+    private let destination = UILabel().then {
+        $0.attributedText = .anza_b2(text: "무슨행", color: .anzaBlack)
+    }
+    private let time = UILabel().then {
+        $0.attributedText = .anza_b2(text: "몇분 몇초", color: .anzaRed)
+    }
+    private let congestion = UIImageView().then {
+        $0.image = UIImage(named: "ic_enough_path")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setUI()
+    }
+}
+
+extension InformationView {
+    func configure(with model: Information) {
+        destination.attributedText = .anza_b2(text: model.destination, color: .anzaBlack)
+        time.attributedText = .anza_b2(text: model.time, color: .anzaBlack)
+        congestion.image = UIImage(named: model.congestion)
+    }
+    func setUI() {
+        [destination, time, congestion].forEach { addSubview($0) }
+        congestion.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        time.snp.makeConstraints { make in
+            make.centerY.equalTo(congestion)
+            make.trailing.equalTo(congestion.snp.leading).offset(-9)
+        }
+        destination.snp.makeConstraints { make in
+            make.centerY.equalTo(congestion)
+            make.leading.equalToSuperview()
+        }
+    }
+}
